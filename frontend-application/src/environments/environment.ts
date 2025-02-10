fe923fafc01d7bb61018053ded3a216a43b4a3b6
ichrak-starr <114824716+ichrak-starr@users.noreply.github.com>
2025-02-10 09:59:11 +0100

    Update environment.ts

--- frontend-application/src/environments/environment.ts
+++ frontend-application/src/environments/environment.ts
@@ -1,4 +1,4 @@
 export const environment = {
   production: false,
-  baseUrl: 'http://backend-service:8080', //a changer par nom de service url de service (ip:32000)
+  baseUrl: 'http://172.31.3.50:30081', //a changer par nom de service url de service (ip:32000)
 };
